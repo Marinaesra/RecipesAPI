daa06ab58768d04e60a81a90ddc8cf77c6cd70aa
Marinaesra <Marina_esra@hotmail.com>
2025-05-23 18:24:12 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+//Crear una aplicacion de Express
+const app = express();
+
+//Middleware básico para analizar JSON en las solicitudes
+app.use(express.json());
+
+//Configuracion del puerto
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+//Configuracion del puerto
+const PORT = 3000;
+
+//Inciar el servidor 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
